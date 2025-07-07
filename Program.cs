42580ad59b12ddd00fec714818b1537b7cd01e9f
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-07 17:34:09 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿//int first = 2;
+//string second = "4";
+//int result = first + second;
+//Console.WriteLine(result); //C:\Users\someuser\Desktop\csharpprojects\TestProject\Program.cs(3,14): error CS0029: Cannot implicitly convert type 'string' to 'int'
+
+//int first = 2;
+//string second = "4";
+//string result = first + second;
+//Console.WriteLine(result); //24
+
+
+
+//int myInt = 3;
+//Console.WriteLine($"int: {myInt}");
+
+//decimal myDecimal = myInt;
+//Console.WriteLine($"decimal: {myDecimal}");
+
+//decimal myDecimal = 3.14m;
+//Console.WriteLine($"decimal: {myDecimal}");
+
+//int myInt = (int)myDecimal;
+//Console.WriteLine($"int: {myInt}");
+
+//decimal myDecimal = 1.23456789m;
+//float myFloat = (float)myDecimal;
+
+//Console.WriteLine($"Decimal: {myDecimal}");
+//Console.WriteLine($"Float  : {myFloat}");
+//Console.WriteLine($"float: {float.MinValue}");
+
+
+//int first = 5;
+//int second = 7;
+//string message = first.ToString() + second.ToString();
+//Console.WriteLine(message);
+
+//string first = "5";
+//string second = "7";
+//int sum = int.Parse(first) + int.Parse(second);
+//Console.WriteLine(sum);
+
+
+//string value1 = "5";
+//string value2 = "7";
+//int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+//Console.WriteLine(result);
+
+//int value = (int)1.5m; // casting truncates
+//Console.WriteLine(value);
+
+//int value2 = Convert.ToInt32(1.5m); // converting rounds up
+//Console.WriteLine(value2);
+
+//string value = "102";
+//int result = 0;
+//if (int.TryParse(value, out result))
+//{
+//    Console.WriteLine($"Measurement: {result}");
+//}
+//else
+//{
+//    Console.WriteLine("Unable to report the measurement.");
+//}
+
+//string value = "bad";
+//int result = 0;
+//if (int.TryParse(value, out result))
+//{
+//    Console.WriteLine($"Measurement: {result}");
+//}
+//else
+//{
+//    Console.WriteLine("Unable to report the measurement.");
+//}
+
+//if (result > 0)
+//    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+//double total = 0;
+//string message = "";
+
+//foreach (var item in values)
+//{
+//    double number;
+//    if (double.TryParse(item, out number))
+//    {
+//        total += number;
+//    }
+//    else
+//    {
+//        message += item;
+//    }
+//}
+
+//Console.WriteLine($"Message: {message}");
+//Console.WriteLine($"Total: {total}");
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+int result1 = Convert.ToInt32(value1 / value2); 
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
